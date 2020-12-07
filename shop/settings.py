7d5c98237b85e8e97a7efdd8dfd45105ab6a6c77
deft727 <deft727@gmail.com>
2020-12-07 23:15:06 +0200

    make it better

--- shop/settings.py
+++ shop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rm^8ds7_w+kh2m%2_my$rb!s9-dg)cnqk+95yzfw(asx=-b&gb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
