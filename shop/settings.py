9820a34b40bd15d264d909cdde9024c43dd92c47
deft727 <deft727@gmail.com>
2020-12-23 00:50:07 +0200

    make it better

--- shop/settings.py
+++ shop/settings.py
@@ -136,9 +136,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static_dev'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR,'static_dev'),
+# )
 
 CRISPY_TEMPLATE_PACK='bootstrap3'
 
