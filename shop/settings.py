4f1f09c06dc9ca3ccfe7706f1fe8ca8820d2d6c2
deft727 <deft727@gmail.com>
2021-01-14 00:36:27 +0200

    added pagination

--- shop/settings.py
+++ shop/settings.py
@@ -166,7 +166,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'mainapp/static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
