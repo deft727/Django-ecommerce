29a3a2ee997670c487b2399cb0281c1f05a13b59
deft727 <deft727@gmail.com>
2021-01-14 12:08:32 +0200

    added pagination

--- shop/settings.py
+++ shop/settings.py
@@ -154,16 +154,16 @@ USE_TZ = True
 
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'mainapp/static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '/static/')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mainapp/static/media')
+MEDIA_URL = '/mainapp/static/media/'
 
 
 
