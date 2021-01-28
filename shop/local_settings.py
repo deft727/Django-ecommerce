774596b83e12ccafc67fb0bf9e68d11be571b40d
deft727 <deft727@gmail.com>
2021-01-28 13:15:12 +0200

    added pagination

--- shop/local_settings.py
+++ shop/local_settings.py
@@ -4,9 +4,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'rm^8ds7_w+kh2m%2_my$rb!s9-dg)cnqk+95yzfw(asx=-b&gb'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1","mysite123456.herokuapp.com"]
 
 DATABASES = {
     'default': {
@@ -17,3 +17,6 @@ DATABASES = {
 
 STATIC_DIR =  os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS =  [STATIC_DIR]
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
