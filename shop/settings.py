4c9d27cdb931c0e7dd007609362f32fefab8cb93
deft727 <deft727@gmail.com>
2021-01-28 13:50:25 +0200

    added pagination

--- shop/settings.py
+++ shop/settings.py
@@ -109,12 +109,12 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 # if os.path.isfile(dotenv_file):
 #     dotenv.load_dotenv(dotenv_file)
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
@@ -197,8 +197,8 @@ EMAIL_HOST_PASSWORD ='tiaxxhsvgnvmjyne'
 
 
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 django_heroku.settings(locals())
 
