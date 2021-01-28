a8dc1072f103f8a3792f3fab07e466d7fa3bf161
deft727 <deft727@gmail.com>
2021-01-28 13:36:56 +0200

    added pagination

--- shop/prod_settings.py
+++ shop/prod_settings.py
@@ -27,8 +27,9 @@ DATABASES = {
     }
 }
 
-STATIC_DIR =  os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS =  [STATIC_DIR]
-STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+
+STATIC_DIR =  os.path.join(BASE_DIR, 'shop/mainapp/static')
+# STATICFILES_DIRS =  os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT =  os.path.join(BASE_DIR, 'shop/mainapp/static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- shop/settings.py
+++ shop/settings.py
@@ -174,10 +174,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATIC_DIR =  os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS =  [STATIC_DIR]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATIC_DIR =  os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS =  [STATIC_DIR]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
